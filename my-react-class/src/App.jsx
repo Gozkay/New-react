5607f379369f5a9bcde734b60268b1831d01895a
Chigozie Emmanuel <stompiddo3@email.com>
2026-04-23 12:18:22 +0100

    first commit

---
+++ my-react-class/src/App.jsx
@@ -0,0 +1,20 @@
+import Header from "./header"
+import Footer from "./footer"
+import Food from "./food"
+import Card from "./card"
+
+function App() {
+  return (<>
+    <Header />
+    <p>This is my app.</p>
+    <Food />  
+    <Card />
+    <Footer />
+          
+  </>
+  )
+}
+ 
+
+
+export default App
---
+++ my-react-class/src/card.jsx
@@ -0,0 +1,11 @@
+import Picture from "./assets/nude.jpg"
+function Card() {
+    return (
+    <div className="card">
+        <img src={Picture} alt="Card Image" />
+        <h2>Card Title</h2>
+        <p>This is a card description.</p>
+    </div>  
+    ) 
+}
+export default Card
